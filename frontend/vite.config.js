6603d529944762bf5cda092d79a2fcb570de0189
Clove Twilight <admin@clovetwilight3.co.uk>
2025-04-13 22:49:06 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
+    port: 8080,
     allowedHosts: ['friends.clovetwilight3.co.uk', 'localhost', '127.0.0.1'],
   },
   plugins: [react()],
