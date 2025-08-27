f58dda46c3bad811dde83fb445273be8b84a1832
𖤐 Clove 𖤐 Twilight <clovetwilight3@outlook.com>
2025-08-27 04:38:10 +0100

    Update Welcome.jsx

--- frontend/src/Welcome.jsx
+++ frontend/src/Welcome.jsx
@@ -30,7 +30,7 @@ const Welcome = ({ loggedIn, isAdmin }) => {
   const [imageError, setImageError] = useState(false);
   
   // Default avatar for fallback
-  const defaultAvatar = "https://www.alextlm.co.uk/system.png";
+  const defaultAvatar = "https://doughmination.win/common/fallback_avatar.png";
   
   useEffect(() => {
     if (loggedIn) {
